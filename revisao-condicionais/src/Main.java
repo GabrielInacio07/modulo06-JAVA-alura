6ea163eff38943dc4371c3348e77190af9ebc15c
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-05 00:36:05 -0300

    Terceiro exercicio da recapitulação verificar a senha digitada com uma senha já setada em outra variavel.

--- revisao-condicionais/src/Main.java
+++ revisao-condicionais/src/Main.java
@@ -4,27 +4,19 @@ class Main{
     static void main(String[] args) {
         System.out.println("Revisão condicionais [Módulo 06]");
         Scanner input = new Scanner(System.in);
+        String password = "123456";
 
-        System.out.println("Boletim Escolar");
-        System.out.println("Digite a nota do primeiro bimestre: ");
-        Double nota = Double.parseDouble(input.nextLine());
+        System.out.println("Validador de senha");
 
-        System.out.println("Digite a nota do segundo bimestre: ");
-        Double nota2 = Double.parseDouble(input.nextLine());
+        System.out.println("Digite sua senha: ");
+        String senhaDigitada = input.nextLine();
 
-        System.out.println("Digite a nota do terceiro bimestre: ");
-        Double nota3 = Double.parseDouble(input.nextLine());
-
-        double media = (nota + nota2 + nota3) / 3;
-        String mediaFormatada = String.format("%.2f",media);
-
-        if(media >= 8.2){
-            System.out.println("O estudante teve a média de: " + mediaFormatada + " e foi aprovado");
-        }else if(media >= 5.8){
-            System.out.println("O estudante teve a média de: " + mediaFormatada + " e está de recuperação");
-        }else{
-            System.out.println("O estudante teve a média de: " + mediaFormatada + " e foi reprovado");
+        if(!senhaDigitada.equals(password)){
+            System.out.println("Senha inválida");
+        }else {
+            System.out.println("Acesso permitido!");
         }
 
+        input.close();
     }
 }
