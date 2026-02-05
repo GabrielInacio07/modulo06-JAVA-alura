5a220ac831883587ab7253ab8751fe6e8f424d49
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-04 23:59:13 -0300

    Primeiro exercicio da recapitulação verificar se um número é PAR ou IMPAR

--- revisao-condicionais/src/Main.java
+++ revisao-condicionais/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 class Main{
     static void main(String[] args) {
-        System.out.println("Revisão condicionais");
+        System.out.println("Revisão condicionais [Módulo 06]");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite um número para validar: ");
+        int number = Integer.parseInt(input.nextLine());
+
+        if(number % 2 == 0){
+            System.out.println("Número digitado é [PAR]");
+        }else{
+            System.out.println("Número digitado é [IMPAR]");
+        }
     }
 }
