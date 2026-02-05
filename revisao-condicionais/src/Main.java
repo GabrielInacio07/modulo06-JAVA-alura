5c58bb253e3d7eaaaf683ddfa4365ba2dd257263
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-05 00:13:47 -0300

    Segunda exercicio da recapitulação verificar as médias de um boletim pegar os valores do exercicios e comparar

--- revisao-condicionais/src/Main.java
+++ revisao-condicionais/src/Main.java
@@ -5,13 +5,26 @@ class Main{
         System.out.println("Revisão condicionais [Módulo 06]");
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Digite um número para validar: ");
-        int number = Integer.parseInt(input.nextLine());
+        System.out.println("Boletim Escolar");
+        System.out.println("Digite a nota do primeiro bimestre: ");
+        Double nota = Double.parseDouble(input.nextLine());
 
-        if(number % 2 == 0){
-            System.out.println("Número digitado é [PAR]");
+        System.out.println("Digite a nota do segundo bimestre: ");
+        Double nota2 = Double.parseDouble(input.nextLine());
+
+        System.out.println("Digite a nota do terceiro bimestre: ");
+        Double nota3 = Double.parseDouble(input.nextLine());
+
+        double media = (nota + nota2 + nota3) / 3;
+        String mediaFormatada = String.format("%.2f",media);
+
+        if(media >= 8.2){
+            System.out.println("O estudante teve a média de: " + mediaFormatada + " e foi aprovado");
+        }else if(media >= 5.8){
+            System.out.println("O estudante teve a média de: " + mediaFormatada + " e está de recuperação");
         }else{
-            System.out.println("Número digitado é [IMPAR]");
+            System.out.println("O estudante teve a média de: " + mediaFormatada + " e foi reprovado");
         }
+
     }
 }
